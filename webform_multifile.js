2c30a6e6d32485443aa981f069828bca7ebb988d
Scott Hadfield <hadsie@gmail.com>
2011-04-30 17:41:44 +0200

    fixing js hardcoded element id

--- webform_multifile.js
+++ webform_multifile.js
@@ -1,11 +1,5 @@
 
-Drupal.behaviors.webform_multiple = function (context) {
-
-    var id = '#webform-component-multi-file-upload';
-    $(id + ' input').attr('name', $(id + ' input').attr('name') + '[]');
-    /*
-    $('#webform-component-multi-file-upload input').MultiFile({
-	namePattern: "files[multi_file_upload][]",
-	list: id + ' .inner'
-	*/
+Drupal.behaviors.webform_multifile = function (context) {
+  var id = '#' + Drupal.settings.webform_multifile.id;
+  $(id + ' input').attr('name', $(id + ' input').attr('name') + '[]');
 }
