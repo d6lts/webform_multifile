356554fe20892be54557af49b7591cf167e80c99
Peter Droogmans <peter@attiks.com>
2011-11-08 21:56:06 +0100

    Issue #1145490 by attiks: Fixed Files are not saved. Issue #1246794 by cyborg_572: Added Multiple multifile fields, and scripts in the footer.

--- webform_multifile.js
+++ webform_multifile.js
@@ -1,5 +1,15 @@
-
 Drupal.behaviors.webform_multifile = function (context) {
-  var id = '#' + Drupal.settings.webform_multifile.id;
-  $(id + ' input').attr('name', $(id + ' input').attr('name') + '[]');
+  // Re-initialize multifile fields with the proper settings
+  if(typeof MultiFile_fields !== 'undefined'){
+    for(var i=0; i<MultiFile_fields.length; i++){
+      $('input[type=file].multi.'+MultiFile_fields[i].id).MultiFile(MultiFile_fields[i].properties);
+    }
+  }
+  
+  // Add the extra brakets to all the Multifile fields
+  $("input[type=file].multi").each(function(){
+    var field = $(this);
+    field.attr('name', field.attr('name') + '[]');
+  });
 }
+
