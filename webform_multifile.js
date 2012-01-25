3d6c8ae901998ad998ba95be6eddde10cf64bdad
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2012-01-25 14:37:20 +0100

    port to D7 from 6.x-1.x-dev

--- webform_multifile.js
+++ webform_multifile.js
@@ -1,14 +1,18 @@
-Drupal.behaviors.webform_multifile = function (context) {
-  // Re-initialize multifile fields with the proper settings
-  if(typeof MultiFile_fields !== 'undefined'){
-    for(var i=0; i<MultiFile_fields.length; i++){
-      $('input[type=file].multi.'+MultiFile_fields[i].id).MultiFile(MultiFile_fields[i].properties);
+(function ($) {
+  Drupal.behaviors.webform_multifile = {
+    attach: function (context) {
+      // Re-initialize multifile fields with the proper settings
+      if(typeof MultiFile_fields !== 'undefined'){
+        for(var i=0; i<MultiFile_fields.length; i++){
+          $('input[type=file].multi.'+MultiFile_fields[i].id).MultiFile(MultiFile_fields[i].properties);
+        }
+      }
+
+      // Add the extra brakets to all the Multifile fields
+      $("input[type=file].multi").each(function(){
+        var field = $(this);
+        field.attr('name', field.attr('name') + '[]');
+      });
     }
   }
-
-  // Add the extra brakets to all the Multifile fields
-  $("input[type=file].multi").each(function(){
-    var field = $(this);
-    field.attr('name', field.attr('name') + '[]');
-  });
-}
+} (jQuery));
